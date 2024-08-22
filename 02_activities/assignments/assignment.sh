c25a087e518692452c37821617c9dec00f99ebeb
Erfan Nazari <erfannazari@gmail.com>
2024-08-21 20:02:30 -0400

    committed from pc

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -15,6 +15,7 @@ or
 mkdir dir1 dir2 dir3 dir4 dir5
 
 # 2. List the contents of the parent directory to verify the presence of the 5 directories
+ls 
 
 # 3. Create 5 text files in dir2 named file1, file2, file3, file4, and file5
 
